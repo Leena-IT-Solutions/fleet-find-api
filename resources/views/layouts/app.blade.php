96d766461c24b2bc154495209b56bcb26f0d5171
sandeep8558 <sandeep198558@gmail.com>
2026-07-12 00:44:47 +0530

    Update app layout header to be full width aligning with content area

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                 <!-- Page Heading -->
                 @if (isset($header))
                     <header class="bg-white border-b border-slate-200">
-                        <div class="max-w-7xl mx-auto py-5 px-6 sm:px-8 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
+                        <div class="w-full py-5 px-6 sm:px-8 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4">
                             <div>
                                 {{ $header }}
                             </div>
